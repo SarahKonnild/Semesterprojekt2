0efe1ff0dc7e5131b75412a9288456880de42ca3
Teyson <teis@aalbaek-nielsen.dk>
2020-04-14 13:56:52 +0200

    Persistence stuff

---
+++ CredIT/src/main/java/org/openjfx/interfaces/IPersistence.java
@@ -0,0 +1,34 @@
+package interfaces;
+
+import java.util.List;
+
+public interface IPersistence {
+
+    public boolean createUser(IUser user);
+
+    public boolean deleteUser(int id);
+
+    public boolean createBroadcast(IBroadcast broadcast);
+
+    public boolean deleteBroadcast(int id);
+
+    public boolean createProduction(IProduction production);
+
+    public boolean removeProduction(int id);
+
+    public boolean createCast(ICast cast);
+
+    public boolean removeCast(int id);
+
+    public List<String> getBroadcast(String keyword);
+
+    public List<String> getCast(String keyword);
+
+    public List<String> getProduction(String keyword);
+
+    public boolean mergeCast(ICast cast1, ICast cast2);
+
+    public boolean update(int id, String name, int regDKID);
+
+
+}
--- CredIT/src/main/java/org/openjfx/persistence/Persistence.java
+++ CredIT/src/main/java/org/openjfx/persistence/Persistence.java
@@ -1,4 +1,142 @@
-package org.openjfx.persistence;
+package Persistence;
 
-public class Persistence {
+import interfaces.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Scanner;
+
+public class Persistence implements IPersistence {
+
+
+    private File castFile;
+    private File productionFile;
+    private File broadcastFile;
+    private File userFile;
+    private PrintWriter writer;
+    private Scanner reader;
+
+    static Persistence instance = null;
+
+
+
+    private Persistence(){
+        this.castFile = new File("PersistenceFiles\\castFile.csv");
+        this.broadcastFile = new File("PersistenceFiles\\broadcastFile.csv");
+        this.productionFile = new File("PersistenceFiles\\productionFile.csv");
+        this.userFile = new File("PersistenceFiles\\userFile.csv");
+        this.writer = null;
+        this.reader = null;
+    }
+
+
+    @Override
+    public boolean createUser(IUser user) {
+        try {
+            writer = new PrintWriter(userFile);
+
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            writer.close();
+        }
+
+
+        return false;
+    }
+
+    @Override
+    public boolean deleteUser(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean createBroadcast(IBroadcast broadcast) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteBroadcast(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean createProduction(IProduction production) {
+        return false;
+    }
+
+    @Override
+    public boolean removeProduction(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean createCast(ICast cast) {
+        return false;
+    }
+
+    @Override
+    public boolean removeCast(int id) {
+        return false;
+    }
+
+    @Override
+    public List<String> getBroadcast(String keyword) {
+        try {
+            reader = new Scanner(broadcastFile);
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            reader.close();
+        }
+
+
+        return null;
+    }
+
+    @Override
+    public List<String> getCast(String keyword) {
+        return null;
+    }
+
+    @Override
+    public List<String> getProduction(String keyword) {
+        return null;
+    }
+
+    @Override
+    public boolean mergeCast(ICast cast1, ICast cast2) {
+        return false;
+    }
+
+    @Override
+    public boolean update(int id, String name, int regDKID) {
+        return false;
+    }
+
+
+    public static Persistence getInstance() {
+        if(instance == null){
+            instance = new Persistence();
+        }
+
+        return instance;
+    }
+
+    public static void main(String[] args){
+        Persistence persistence = Persistence.getInstance();
+        persistence.deleteBroadcast(19);
+    }
+
 }
+
+
+
