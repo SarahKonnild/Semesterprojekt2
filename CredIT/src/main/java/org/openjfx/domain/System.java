afe9fd0b162127287ac12758f5bf74caf39f5b90
TrickyMaker <sikri19@student.sdu.dk>
2020-04-14 14:01:56 +0200

    added the skeleton for the user class

--- CredIT/src/main/java/org/openjfx/domain/System.java
+++ CredIT/src/main/java/org/openjfx/domain/System.java
@@ -1,4 +1,37 @@
-package org.openjfx.domain;
+package domain;
 
-public class System {
+import interfaces.ISystem;
+
+public class System //implements ISystem
+         {
+    private User user;
+    private int persistenceLayer;
+
+    public System(User user, int persistenceLayer){
+        this.user = user;
+        this.persistenceLayer = persistenceLayer;
+    }
+
+/*
+    @Override
+    public User createUser(int id, String name, String password, String username, String role) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Cast> searchCast(String keyword) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Broadcast> searchBroadcast(String keyword) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Production> searchProduction(String keyword) {
+        return null;
+    }
+
+ */
 }
---
+++ CredIT/src/main/java/org/openjfx/domain/User.java
@@ -0,0 +1,47 @@
+package domain;
+
+import interfaces.IUser;
+
+import java.util.Date;
+
+public class User implements IUser{
+    private int id;
+    private String name;
+    private String password;
+    private String username;
+    private Role role;
+
+    private enum Role{
+        SYSADMIN, NETWORK, PRODUCER;
+    }
+
+    public User(int id, String name, String password, String username, Role role){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.username = username;
+        this.role = role;
+    }
+    /*
+    @Override
+    public Cast addNewCast(String name, int regDKID) {
+        return null;
+    }
+
+    @Override
+    public boolean checkRole(String role) {
+        return false;
+    }
+
+    @Override
+    public Production addNewProduction(String name, Date year, String productionCompany) {
+        return null;
+    }
+
+    @Override
+    public Broadcast addNewBroadcast(Production production, String name, int seasonNumber, int episodeNumber, Date airDate) {
+        return null;
+    }
+
+ */
+}
