a53325ffaa4c1484c9a664276896bd05d63bdfa8
Nichlas Boraso <nibor19@student.sdu.dk>
2020-04-16 14:32:19 +0200

    test fixes

--- CredIT/src/main/java/org/openjfx/interfaces/IProduction.java
+++ CredIT/src/main/java/org/openjfx/interfaces/IProduction.java
@@ -9,8 +9,8 @@ public interface IProduction {
     public boolean saveProduction(Production production);
     public int getId();
     public String getName();
-    public int getYear();
-    public ArrayList<Broadcast> getBroadcasts();
+    public String getYear();
+    public ArrayList<IBroadcast> getBroadcasts();
     public String getProductionCompany();
     public int getNumberOfSeasons();
     public int getNumberOfEpisodes();
