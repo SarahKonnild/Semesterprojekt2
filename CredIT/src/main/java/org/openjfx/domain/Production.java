901c99e3dd0b9d94c363662f388b1e347b3f1c83
Simon Quvang <simon.quvang@gmail.com>
2020-05-12 11:20:59 +0200

    Cherrypick: Added assign and unassign broadcast to production

--- CredIT/src/main/java/org/openjfx/domain/Production.java
+++ CredIT/src/main/java/org/openjfx/domain/Production.java
@@ -5,17 +5,16 @@ import org.openjfx.interfaces.IPersistence;
 import org.openjfx.interfaces.IProduction;
 import org.openjfx.interfaces.IProductionCompany;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Production implements IProduction {
     private final IPersistence persistence = CredITSystem.getPersistence();
     private final CredITSystem system = CredITSystem.getInstance();
+    private final IProductionCompany productionCompany;
     private int id;
     private String name;
     private String year;
     private ArrayList<IBroadcast> broadcasts;
-    private IProductionCompany productionCompany;
     private int numberOfSeasons;
     private int numberOfEpisodes;
 
@@ -38,11 +37,11 @@ public class Production implements IProduction {
      * Calls the search method in System, to get an arraylist of the broadcasts that this production have associted with it.
      */
 
-    private void loadBroadcastArray(){
+    private void loadBroadcastArray() {
         this.broadcasts = system.searchBroadcast(this.id);
         int tempSeasonNumber = broadcasts.get(0).getSeasonNumber();
-        for(IBroadcast broadcast : this.broadcasts){
-            if(tempSeasonNumber < broadcast.getSeasonNumber()){
+        for (IBroadcast broadcast : this.broadcasts) {
+            if (tempSeasonNumber < broadcast.getSeasonNumber()) {
                 tempSeasonNumber = broadcast.getSeasonNumber();
             }
         }
@@ -53,8 +52,8 @@ public class Production implements IProduction {
     @Override
     public boolean save() {
         int idNumber = persistence.createNewProductionInDatabase(this);
-        if(idNumber != -1) this.id = idNumber;
-        return (idNumber == -1) ? false : true;
+        if (idNumber != -1) this.id = idNumber;
+        return idNumber != -1;
     }
 
     @Override
@@ -69,14 +68,48 @@ public class Production implements IProduction {
 
         this.name = name;
         this.year = year;
-        if(persistence.updateProduction(this))
+        if (persistence.updateProduction(this))
             return true;
-        else
-        {
+        else {
             this.name = tempName;
             this.year = tempYear;
             return false;
         }
+    }
+
+    @Override
+    public boolean assignBroadcast(IBroadcast broadcast) {
+        ArrayList<IBroadcast> tempList = this.broadcasts;
+        if (this.broadcasts.contains(broadcast)) {
+            return true;
+
+        } else {
+            this.broadcasts.add(broadcast);
+            if (persistence.updateProduction(this))
+                return true;
+            else {
+                this.broadcasts = tempList;
+                return false;
+            }
+        }
+    }
+
+    @Override
+    public boolean unassignBroadcast(IBroadcast broadcast) {
+        if (this.broadcasts.contains(broadcast)) {
+            //Saves the list so it can revert back if anything goes wrong.
+            ArrayList<IBroadcast> tempList = this.broadcasts;
+            this.broadcasts.remove(broadcast);
+            if (persistence.updateProduction(this))
+                return true;
+            else {
+                //Reverts the changes because something went wrong
+                this.broadcasts = tempList;
+                return false;
+            }
+
+        } else
+            return false;
     }
 
     @Override
@@ -98,18 +131,22 @@ public class Production implements IProduction {
     public String getYear() {
         return this.year;
     }
+
     @Override
     public ArrayList<IBroadcast> getBroadcasts() {
         return this.broadcasts;
     }
+
     @Override
     public IProductionCompany getProductionCompany() {
         return this.productionCompany;
     }
+
     @Override
     public int getNumberOfSeasons() {
         return this.numberOfSeasons;
     }
+
     @Override
     public int getNumberOfEpisodes() {
         return this.numberOfEpisodes;
